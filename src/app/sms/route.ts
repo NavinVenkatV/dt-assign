8863070532da2f565b895b3f7fbb708ef422b7dc
NavinVenkatV <vnavinvenkat@gmail.com>
2025-04-15 03:14:34 +0530

    init

--- src/app/sms/route.ts
+++ src/app/sms/route.ts
@@ -1,22 +1,28 @@
 import { NextRequest, NextResponse } from "next/server";
 import twilio from "twilio";
 
+
 const client = twilio(
   process.env.TWILIO_ACCOUNT_SID,
   process.env.TWILIO_AUTH_TOKEN
 );
 
 export async function POST(req: NextRequest) {
+  console.log(process.env.TWILIO_ACCOUNT_SID)
   const { to, text } = await req.json();
   const number = `+${to.trim()}`;
-  const accountSid = 'AC24965f43d2f864cde1e406c01798a149';
-const authToken = 'f25faa82b3acd488ad976774108bcb20';
-const client = require('twilio')(accountSid, authToken);
-client.messages
-    .create({
+  const accountSid = process.env.TWILIO_ACCOUNT_SID;
+  const authToken = process.env.TWILIO_AUTH_TOKEN;
+  try {
+    const client = require('twilio')(accountSid, authToken);
+    client.messages
+      .create({
         body: text,
-        from: '+17156465239',
+        from: process.env.TWILIO_PHONE_NUMBER,
         to: number
-    })
-    return NextResponse.json({message : "Sent Successfully"})
+      })
+    return NextResponse.json({ message: "Sent Successfully" })
+  } catch (e) {
+    return NextResponse.json({ message: "Somthing went wrong " })
+  }
 }
